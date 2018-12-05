3d711ffe10d57186e30322cbe92394e9e9614b72
Josh Gude <jgude@Joshs-MacBook-Pro.local>
2018-12-05 01:22:34 -0500

    backend complete, frontend started

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jorygu:jrg192567@ds263759.mlab.com:63759/devconnector-jorygu'
+  mongoURI: 'mongodb://jorygu:jrg192567@ds263759.mlab.com:63759/devconnector-jorygu',
+  secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({exteded: false}));
+app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
@@ -12,12 +17,14 @@ mongoose.connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
